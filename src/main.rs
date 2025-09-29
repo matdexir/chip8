8b47a50991b4af6036692b7d5ef757e2d5707389
matdexir <matthieudesir21@gmail.com>
2025-09-29 18:01:54 +0800

    some frontend

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 mod vm;
 use raylib::prelude::*;
+use std::env;
+
+use crate::vm::{SCREEN_HEIGHT, SCREEN_WIDTH};
+
+const SCALE: u32 = 15;
+const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
 
 fn main() {
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("usage: cargo run path/to/rom")
+        return;
+    }
+
     let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
 
     while !rl.window_should_close() {
