783148892a258dea43b296175ed6e2fe3b5e837f
matdexir <matthieudesir21@gmail.com>
2025-09-29 15:54:48 +0800

    add basic operations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod vm;
 use raylib::prelude::*;
 
 fn main() {
---
+++ src/vm.rs
@@ -0,0 +1,86 @@
+const RAM_SIZE: usize = 4096;
+const REGISTER_COUNT: usize = 16;
+const STACK_SIZE: usize = 16;
+const KEYS_COUNT: usize = 16;
+
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
+const START_ADDR: u16 = 0x200;
+
+const FONTSET_SIZE: usize = 80;
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+pub struct Chip8VM {
+    pc: u16,
+    memory: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    registers: [u8; REGISTER_COUNT],
+    i_register: u16,
+    sp: u16,
+    stack: [u16; STACK_SIZE],
+    keys: [bool; KEYS_COUNT],
+    delay_timer: u8,
+    sound_timer: u8,
+}
+
+impl Chip8VM {
+    pub fn new() -> Self {
+        let mut chip8vm = Chip8VM {
+            pc: START_ADDR,
+            memory: [0; RAM_SIZE],
+            screen: [false; SCREEN_HEIGHT * SCREEN_WIDTH],
+            registers: [0; REGISTER_COUNT],
+            i_register: 0,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; KEYS_COUNT],
+            delay_timer: 0,
+            sound_timer: 0,
+        };
+
+        chip8vm.memory[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+        return chip8vm;
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = START_ADDR;
+        self.memory = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_HEIGHT * SCREEN_WIDTH];
+        self.registers = [0; REGISTER_COUNT];
+        self.i_register = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; KEYS_COUNT];
+        self.delay_timer = 0;
+        self.sound_timer = 0;
+    }
+
+    fn push_to_stack(self: &mut Self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    fn pop_from_stack(self: &mut Self) -> u16 {
+        let val = self.stack[(self.sp - 1) as usize];
+        self.sp -= 1;
+        return val;
+    }
+}
