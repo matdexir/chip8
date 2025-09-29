7a9d08e0af5b2453a28748d9b6c49b72b5b0f7d0
matdexir <matthieudesir21@gmail.com>
2025-09-29 18:03:59 +0800

    fix some formatting issues

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,22 @@ use std::env;
 
 use crate::vm::{SCREEN_HEIGHT, SCREEN_WIDTH};
 
-const SCALE: u32 = 15;
-const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
-const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const SCALE: i32 = 15;
+const WINDOW_WIDTH: i32 = (SCREEN_WIDTH as i32) * SCALE;
+const WINDOW_HEIGHT: i32 = (SCREEN_HEIGHT as i32) * SCALE;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
 
     if args.len() != 2 {
-        println!("usage: cargo run path/to/rom")
+        println!("usage: cargo run path/to/rom");
         return;
     }
 
-    let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
+    let (mut rl, thread) = raylib::init()
+        .size(WINDOW_WIDTH, WINDOW_HEIGHT)
+        .title("Chip 8 EMU")
+        .build();
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
